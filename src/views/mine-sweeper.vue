0998fd1ab1eccb26a3dd1ba186eb7ea217769714
BinarySky10 <yilingxiankong@hotmail.com>
2022-03-13 14:46:06 +0800

    0

--- src/views/mine-sweeper.vue
+++ src/views/mine-sweeper.vue
@@ -1,6 +1,21 @@
+<script lang="ts">
+const width = 10;
+const height = 10;
+
+let array = new Array(height).fill(new Array(width).fill(0));
+declare global {
+  interface Window {
+    something: any;
+  }
+}
+import { ref } from "vue";
+window.something = ref(0);
+
+console.log();
+</script>
 <template>
   <div class="mine-map">
-    <div v-for="y in 10" :key="y" >
+    <div v-for="y in 10" :key="y">
       <button class="mine-block" v-for="x in 10" :key="x">
         {{ (y - 1) * 10 + x }}
       </button>
