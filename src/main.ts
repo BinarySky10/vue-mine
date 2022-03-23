140242d4a357384d32e925da8af592b36f41f00d
BinarySky10 <yilingxiankong@hotmail.com>
2022-03-23 09:47:16 +0800

    配置

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 // 全局样式
 import '@/styles/index.scss'
 // vue
-import { createApp } from 'vue';
-import router from '@/router/index';
-// elementPlus
+import { createApp } from 'vue'
 import elementPlus from 'element-plus'
+import router from '@/router/index'
+// elementPlus
 import 'element-plus/dist/index.css'
 // 主页
-import App from '@/App.vue';
-//图标库
+import App from '@/App.vue'
+// 图标库
 // https://remixicon.com/
 import 'remixicon/fonts/remixicon.css'
-var a = "20";
 createApp(App)
   .use(elementPlus)
   .use(router)
   .mount('#app')
-
